25442c3210d56d310f4af12eb3fc8ad5c71cd7ab
Dmitry <example@gmail.com>
2021-07-15 12:46:42 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log(4 + +'10');
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(incr++);
+console.log(--decr);
+console.log(incr++);
+
+
+console.log(3135%4);
+
+console.log('2*5' == 10);
+
+const isChecked = true,
+    isClose = true;
+console.log(isChecked && isClose);

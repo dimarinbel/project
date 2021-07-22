c51597ec76e9b921a42bc2567eeb3dbaa8850c1c
Dmitry <example@gmail.com>
2021-07-22 12:37:55 +0300

    practic lesson 1

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 "use strict";
 
-console.log(4 + +'10');
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
 
-let incr = 10,
-    decr = 10;
-
-//++incr;
-//--decr;
-
-console.log(incr++);
-console.log(--decr);
-console.log(incr++);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
 
-console.log(3135%4);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
 
-console.log('2*5' == 10);
 
-const isChecked = true,
-    isClose = true;
-console.log(isChecked && isClose);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);

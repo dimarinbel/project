f157ca4e33a7846d34e7b5c66f70b7b81d9b9343
Dmitry <example@gmail.com>
2021-08-25 11:19:47 +0300

    lesson 25

--- js/script.js
+++ js/script.js
@@ -53,15 +53,27 @@ const personalMovieDB = {
         }
     },
     writeYourGenres: function() {
-        for (let i = 1; i <=3; i++) {
-            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        for (let i = 1; i < 2; i++) {
+            // let genre = prompt(`Ваш любимый жанр под номером ${i}`);
 
-            if (genre == '' || genre == null) {
-                console.log('Вы ввели некорректное значение');
-                i--;
-            } else {
-                personalMovieDB.genres[i-1] = genre;
-            }
+            // if (genre == '' || genre == null) {
+            //     console.log('Вы ввели некорректное значение');
+            //     i--;
+            // } else {
+            //     personalMovieDB.genres[i-1] = genre;
+            // }
+
+            let genres = prompt(`Введите Ваши любимые жанры через запятую`).toLowerCase();
+           
+            if (genres == '' || genres == null) {
+                    console.log('Вы ввели некорректное значение');
+                    i--;
+                } else {
+                    personalMovieDB.genres = genres.split(', ');
+                    personalMovieDB.genres.sort();
+                }
+
+
         }
         personalMovieDB.genres.forEach((item, i) => {
             console.log(`Любимый жанр ${i + 1} - это ${item}`);

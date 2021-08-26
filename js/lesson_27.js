c14d6f9e2658a391a768bf09c157a7de94975ed9
Dmitry <example@gmail.com>
2021-08-26 17:06:29 +0300

    lesson 27

---
+++ js/lesson_27.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// let x = 5;
+
+// console.log(x++);
+
+// console.log([] + false - null + true);
+
+// let y = 1;
+// let x = y = 2;
+
+// console.log(x);
+
+// console.log([] + 1 + 2);
+
+// console.log("1"[0]);
+
+// console.log(2 && 1 && null && 0 && undefined);
+
+// И запинается на лжи
+// Или запинается на правде
+
+// console.log(!!(1 && 2) === (1 && 2));
+
+// console.log(null || 2 && 3 || 4);
+
+// let a = [1, 2, 3];
+// let b = [1, 2, 3];
+
+// function result() {
+//     if (a[0] == b[0]) {
+//         console.log("Правда");
+//     }
+//     else {
+//         console.log("Не правда");
+//     }
+// }
+
+// result();
+
+// console.log(+"Infinity");
+
+console.log("яблоко" > "Ёжик");
